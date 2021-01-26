92268d67cf84e873caec5f1145fc74a2f9e6254a
peter <ehrich@cip.ifi.lmu.de>
2021-01-26 12:55:29 +0100

    Pushing changes from laptop

--- workSpace/session_3/task_1.py
+++ workSpace/session_3/task_1.py
@@ -4,7 +4,7 @@ from netvars import setNetVar, getNetVar, initNet
 
 # # assuming there is a network with ssid hotspot1 and password 123456789
 # connect to wifi
-initNet("FRITZ!Box Fon WLAN 7360", "26019589887890601540")
+initNet("Livebox-ABDA", "zaJKaeqxpQPeY9vHWf")
 
 #Setup Potentiometer
 adc = ADC(0)
@@ -17,7 +17,9 @@ try:
   while True:
     setNetVar("peter_ehrich_task_1", str(adc.read()))
     a=getNetVar("peter_ehrich_task_1")
+    print(a)
     beeper.freq(int(a))
     sleep(0.1)
 except KeyboardInterrupt:
   beeper.deinit()
+
