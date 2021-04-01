ecdda17f0b47926f35bf70cd65b65c5f6f8b3506
peter <ehrich@cip.ifi.lmu.de>
2021-04-02 00:09:53 +0200

    Added 2 lighting modes; breathing and police

--- workSpace/glow_orbs/capacitative_touch_test.py
+++ workSpace/glow_orbs/capacitative_touch_test.py
@@ -2,9 +2,10 @@ from machine import Pin, TouchPad, PWM
 from time import sleep
 from netvars import setNetVar, getNetVar, initNet
 import neopixel
+import math
 
 # connect to wifi
-initNet("WLAN-168808", "53476413711784902142")
+#initNet("WLAN-168808", "53476413711784902142")
 print("connected")
 
 # number of leds
@@ -28,7 +29,7 @@ def clearAll():
 def clear(position):
   color(position,0,0,0)
   
-def search(x,delay):
+def search(x):
     if x==0:
       pixels[n-1]=(0,0,0)
     else:
@@ -36,18 +37,46 @@ def search(x,delay):
         
     pixels[x]=(50,50,50)  
     pixels.write()
-    sleep(delay)
+    sleep(0.02)
+    return (search_counter+1)%n
+    
+def breathing(x,breathing_speed):  
+    pixels.fill((int(((math.sin(x*breathing_speed)+1)/2)*255),0,0))  
+    pixels.write()
+    sleep(0.001)
+    return x+=breathing_speed
+    
+def police(toogle):
+  clearAll()
+  
+  if toggle:
+    for i in range(0,12):
+      pixels[i]=(255,0,0)
+  else:
+    for i in range(13,23):
+      pixels[i]=(0,0,255)
+  
+  pixels.write()
+  sleep(0.25)
+  return not toggle
 
 #Capacitative Touch Pins
 touch = TouchPad(Pin(12))
 
+#Variables for different lighting modes
 search_counter=0
+breathing_counter=0
+breathing_speed = 0.15
+toggle = False
+
 try:
   while True: 
     
     if touch.read() < 450:
-      search(search_counter,0.02)
-      search_counter=(search_counter+1)%n
+      #search_counter=search(search_counter)
+      #breathing_counter=breathing(breathing_counter,breathing_speed)
+      toggle = police(toggle)
+      
     else:
       clearAll()
     
@@ -77,6 +106,7 @@ except KeyboardInterrupt:
   clearAll()
   print("done")
   
+
 
 
 
